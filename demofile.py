e3dbf0af62c9dd9c7eeb58c8d0c9b6ebb83eeb3e
Deepthi Kesireddy <deepthikreddy@deepthis-Air.attlocal.net>
2018-09-08 16:56:14 -0500

    adding python file

---
+++ demofile.py
@@ -0,0 +1,58 @@
+''' SI
+p = input('enter p value')
+n = input('enter n value')
+r = input('enter r value')
+
+p = int(p)
+n = int(n)
+r = int(r)
+
+s = p*n*r//100
+print(s)
+
+if statement
+
+age = float(input('enter age'))
+if age >= 18:
+    print('you are an adult')
+
+else:
+    print('not an adult')
+
+
+marks = int(input('enter marks'))
+if marks >= 90:
+    print('grade A')
+elif marks >=80:
+    print('grade B')
+elif marks >= 70:
+    print('grade C')
+
+
+names = ["deep", "rajeev", "sandeep"]
+print(names[1])
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
